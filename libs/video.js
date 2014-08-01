07c419c4fcce5ab3762204c7d678b1086543e345
Joe Longstreet <joelongstreet@gmail.com>
2014-08-01 00:29:34 -0500

    videos can now strip audio, crop, make slowmo, encode as webm, and capture poster

--- libs/video.js
+++ libs/video.js
@@ -1,23 +1,102 @@
 var Q = require('q');
 var path = require('path');
 var spawn = require('child_process').spawn;
-var videoProfiles = [];
 
 
-
-var encodeAsWebm = function(shortCode){
+var optimize = function(shortCode){
   var deferred = Q.defer();
+  var file = path.join(process.cwd(), 'tmp/') + shortCode + '.mp4';
 
-  var webmPath = path.join(process.cwd(), 'tmp/') + shortCode + '.webm';
-  var moviePath = path.join(process.cwd(), 'tmp/') + shortCode + '.mp4';
-  console.log('encoding as webm...');
+  stripAudio({shortCode: shortCode, file: file})
+    .then(cropClip)
+    .then(makeSlowmo)
+    .then(encodeAsWebm)
+    .then(capturePoster)
+    .fail(function(e){
+      console.log(e);
+    })
+    .done(function(x){
+      console.log(x);
+    });
 
-  var args = ['-i', moviePath, '-codec:v', 'libvpx', '-quality', 'good', '-codec:a', 'libvorbis', webmPath];
+  return deferred.promise;
+};
+
+
+var stripAudio = function(opts){
+  var deferred = Q.defer();
+  var noAudioFile = path.join(process.cwd(), 'tmp/') + opts.shortCode + '-no-audio.mp4';
+  var args = ['-i', opts.file, '-an', noAudioFile, '-y'];
+
   var ffmpeg = spawn('ffmpeg', args);
+  console.log('stripping audio...');
+
+  ffmpeg.on('exit', function(){
+    console.log('stripped audio...');
+    deferred.resolve({
+      file: noAudioFile,
+      shortCode: opts.shortCode
+    });
+  });
+
+  return deferred.promise;
+};
+
+
+var cropClip = function(opts){
+  var deferred = Q.defer();
+  var croppedFile = path.join(process.cwd(), 'tmp/') + opts.shortCode + '-cropped.mp4';
+  var args = ['-i', opts.file, '-filter:v', 'crop=720:720:280:0', croppedFile, '-y'];
+
+  var ffmpeg = spawn('ffmpeg', args);
+  console.log('cropping clip...');
+
+  ffmpeg.on('exit', function(){
+    console.log('cropped clip...');
+    deferred.resolve({
+      file: croppedFile,
+      shortCode: opts.shortCode
+    });
+  });
+
+  return deferred.promise;
+};
+
+
+var makeSlowmo = function(opts){
+  var deferred = Q.defer();
+  var slowmoFile = path.join(process.cwd(), 'tmp/') + opts.shortCode + '.mp4';
+  var args = ['-i', opts.file, '-filter:v', 'setpts=4.5*PTS', slowmoFile, '-y'];
+
+  var ffmpeg = spawn('ffmpeg', args);
+  console.log('making it a slowmo frealsy...');
+
+  ffmpeg.on('exit', function(){
+    console.log('made it a slowmo...');
+    deferred.resolve({
+      file: slowmoFile,
+      shortCode: opts.shortCode
+    });
+  });
+
+  return deferred.promise;
+};
+
+
+
+var encodeAsWebm = function(opts){
+  var deferred = Q.defer();
+  var webmPath = path.join(process.cwd(), 'tmp/') + opts.shortCode + '.webm';
+  var args = ['-i', opts.file, '-codec:v', 'libvpx', '-quality', 'good', '-codec:a', 'libvorbis', webmPath, '-y'];
+
+  var ffmpeg = spawn('ffmpeg', args);
+  console.log('encoding as webm...');
 
   ffmpeg.on('exit', function(){
     console.log('encoded as webm...');
-    deferred.resolve(shortCode);
+    opts.webm = webmPath;
+
+    deferred.resolve(opts);
   });
 
   return deferred.promise;
@@ -25,24 +104,25 @@ var encodeAsWebm = function(shortCode){
 
 
 
-var capturePoster = function(shortCode){
+var capturePoster = function(opts){
   var deferred = Q.defer();
+  var posterPath = path.join(process.cwd(), 'tmp/') + opts.shortCode + '.jpg';
+  var args = ['-i', opts.file, '-vframes', '1', '-ss', '00:00:05', posterPath, '-y'];
 
-  var posterPath = path.join(process.cwd(), 'tmp/') + shortCode + '.jpg';
-  var moviePath = path.join(process.cwd(), 'tmp/') + shortCode + '.mp4';
-  console.log('writing thumnail...');
-
-  var args = ['-i', moviePath, '-vframes', '1', '-ss', '00:00:02', posterPath];
   var ffmpeg = spawn('ffmpeg', args);
+  console.log('writing thumnail...');
 
   ffmpeg.on('exit', function(){
     console.log('wrote thumbnail...');
-    deferred.resolve(shortCode);
+    deferred.resolve({
+      poster: posterPath,
+      video: opts.file,
+      shortCode: opts.shortCode
+    });
   });
 
   return deferred.promise;
 };
 
 
-exports.capturePoster = capturePoster;
-exports.encodeAsWebm = encodeAsWebm;
+exports.optimize = optimize;
