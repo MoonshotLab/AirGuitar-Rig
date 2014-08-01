ae7205f63a4e52b78461da6c76ca65b78c1768d5
Joe Longstreet <joelongstreet@gmail.com>
2014-07-31 22:06:26 -0500

    it can encode videos as webm

--- libs/video.js
+++ libs/video.js
@@ -5,8 +5,29 @@ var videoProfiles = [];
 
 
 
+var encodeAsWebm = function(shortCode){
+  var deferred = Q.defer();
+
+  var webmPath = path.join(process.cwd(), 'tmp/') + shortCode + '.webm';
+  var moviePath = path.join(process.cwd(), 'tmp/') + shortCode + '.mp4';
+  console.log('encoding as webm...');
+
+  var args = ['-i', moviePath, '-codec:v', 'libvpx', '-quality', 'good', '-codec:a', 'libvorbis', webmPath];
+  var ffmpeg = spawn('ffmpeg', args);
+
+  ffmpeg.on('exit', function(){
+    console.log('encoded as webm...');
+    deferred.resolve(shortCode);
+  });
+
+  return deferred.promise;
+};
+
+
+
 var capturePoster = function(shortCode){
   var deferred = Q.defer();
+
   var posterPath = path.join(process.cwd(), 'tmp/') + shortCode + '.jpg';
   var moviePath = path.join(process.cwd(), 'tmp/') + shortCode + '.mp4';
   console.log('writing thumnail...');
@@ -24,3 +45,4 @@ var capturePoster = function(shortCode){
 
 
 exports.capturePoster = capturePoster;
+exports.encodeAsWebm = encodeAsWebm;
